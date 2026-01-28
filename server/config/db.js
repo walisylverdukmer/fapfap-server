099878596a56f51aba09d1dffc42cac3a82abc7c
Mini Mac M2-2023 <minimacm2-2023@192.168.1.99>
2026-01-28 12:23:45 +0000

    Activation SSL pour Aiven

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,19 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'fap_fap_db', // INDISPENSABLE pour le login
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 17787, // Utilise le port d'Aiven
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    // --- AJOUT INDISPENSABLE POUR AIVEN ---
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
+// On exporte la version promise pour pouvoir utiliser async/await dans tes controllers
 module.exports = pool.promise();
